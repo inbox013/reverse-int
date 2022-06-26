fe991303daa83bd33ee4949262ded7891209e20a
inbox013 <inbox0130@gmail.com>
2022-06-26 11:22:18 +0300

    feat: made reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+   let pos = Math.abs(n);
+   let str = String(pos);
+   let spl = str.split("");
+   let rev = spl.reverse();
+   let end = rev.join('')
+   return (end);
 }
